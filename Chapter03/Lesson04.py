b1e981c38decf61e164969e7e676cd93a3cd2e74
Arezu <karami.92036@gmail.com>
2020-05-11 19:21:12 +0430

    final Chapter03-lesson04

--- Chapter03/Lesson04.py
+++ Chapter03/Lesson04.py
@@ -1,17 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-
-
-html = urlopen('https://en.wikipedia.org/wiki/Ali_Daei')
-bs = BeautifulSoup(html, 'html.parser')
-for link in bs.find('div', {'id': 'bodyContent'}).find_all(
-        'a', href=re.compile('^(/wiki/)((?!:).)*$')):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
-
-import datetime
 import random
+import datetime
+
 
 random.seed(datetime.datetime.now())
 def getLinks(wikiUrl):
@@ -19,8 +11,11 @@ def getLinks(wikiUrl):
     bs = BeautifulSoup(html, 'html.parser')
     return bs.find('div', {'id':'bodyContent'}).find_all('a',
         href=re.compile('^(/wiki/)((?!:).)*$'))
+
 links = getLinks('/wiki/Ali_Daei')
 while len(links) > 0:
     newPage = links[random.randint(0, len(links)-1)].attrs['href']
     print(newPage)
     links = getLinks(newPage)
+
+
